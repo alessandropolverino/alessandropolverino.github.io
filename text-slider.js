2467a6add4bcf2058def2f3a80a1b9c13a94a7ff
Alessandro__Polverino <alessandropolverino01@gmail.com>
2020-03-16 18:05:37 +0100

    added sliding text

---
+++ text-slider.js
@@ -0,0 +1,65 @@
+const { tween, physics, styler, easing, value } = window.popmotion;
+// var vertical_rotating_words = document.getElementsByClassName("v-sliding-text");
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function verticalIn(word) {
+  tween({
+    from: 0,
+    to: 1,
+    duration: 750
+  }).start(v => word.set("opacity", v));
+  tween({
+    from: -20,
+    to: 0,
+    duration: 750
+  }).start(v => word.set("y", v));
+}
+
+function verticalOut(word) {
+  tween({
+    from: 1,
+    to: 0,
+    duration: 750
+  }).start(v => word.set("opacity", v));
+  tween({
+    from: 0,
+    to: 20,
+    duration: 750
+  }).start(v => word.set("y", v));
+}
+
+// function wordVerticalSlider(word) {
+//   verticalIn(word);
+//   setTimeout(function() {
+//     verticalOut(word);
+//   }, 2000);
+// }
+
+// for (var i = 0; i < vertical_rotating_words.length; i++) {
+//   var vertical_rotating_word = styler(vertical_rotating_words[i]);
+//   wordVerticalSlider(vertical_rotating_word);
+// }
+
+// console.log(vertical_rotating_words[0]);
+// var word = styler(vertical_rotating_words[0]);
+// wordVerticalSlider(word);
+var word_node = document.getElementsByClassName("v-sliding-text");
+async function wordVerticalSlider(node) {
+  console.log(node);
+  for (var i = 0; i < words.length; i++) {
+    console.log(words[i]);
+    node[0].innerText = words[i];
+    word = styler(node[0]);
+    setTimeout(function() {
+      verticalIn(word);
+    }, 1000);
+    setTimeout(function() {
+      verticalOut(word);
+    }, 4000);
+    await sleep(5000);
+  }
+  wordVerticalSlider(node);
+}
